fce27b9359c65919e5797eedadcda76b482a0b4d
aleximageregistry <114698176@qq.com>
2021-07-30 16:49:41 +0800

    '修改了order控制器'

--- order-center/src/main/java/com/mall/ordercenter/controller/OrderController.java
+++ order-center/src/main/java/com/mall/ordercenter/controller/OrderController.java
@@ -1,4 +1,4 @@
-package com.mall.ordercenter.controller;
+﻿package com.mall.ordercenter.controller;
 
 import com.mall.ordercenter.annotion.LoginUser;
 import com.mall.ordercenter.annotion.Order;
@@ -84,23 +84,16 @@ public class OrderController {
                 model.setOrderOp(OrderEnum.build(orderitem.getOrderStatus().intValue()));
                 model.setAftersaleStatus(orderitem.getAftersaleStatus().intValue());
                 model.setActualPrice(orderitem.getActualPrice());
-//              mapdata.clear();
-//              mapdata.put("id",orderitem.getId());
-//              mapdata.put("orderSn",orderitem.getOrderSn());
-//              mapdata.put("orderStatus",orderitem.getOrderStatus());
-//              mapdata.put("actualPrice",orderitem.getActualPrice());
-//              mapdata.put("orderStatusText", OrderEnum.getMessageByCode(orderitem.getOrderStatus().intValue()));
-//              mapdata.put("orderOp", OrderEnum.build(orderitem.getOrderStatus().intValue()));
-//              mapdata.put("aftersaleStatus",orderitem.getAftersaleStatus());
 
-                ChardancemallGroupon groupon = grouponService.getByOrderId(orderitem.getId());
-                if (groupon != null) {
-                    //mapdata.put("IsGroupon",true);
-                    model.setGroupon(true);
-                } else {
-                    //mapdata.put("IsGroupon",false);
-                    model.setGroupon(false);
-                }
+                //调用微服务
+//                ChardancemallGroupon groupon = grouponService.getByOrderId(orderitem.getId());
+//                if (groupon != null) {
+//                    //mapdata.put("IsGroupon",true);
+//                    model.setGroupon(true);
+//                } else {
+//                    //mapdata.put("IsGroupon",false);
+//                    model.setGroupon(false);
+//                }
 
                 List<ChardancemallOrderGoods> orderGoods = orderGoodsService.getByOrderId(orderitem.getOrderSn());
                 List<Map<String, Object>> goodsdata = new ArrayList<>();
@@ -114,20 +107,10 @@ public class OrderController {
                     goodsModel.setSpecifications(ordergooditem.getSpecifications());
                     goodsModel.setPrice(ordergooditem.getPrice());
 
-//                  goodsmap.clear();
-//                  goodsmap.put("id", ordergooditem.getId());
-//                  goodsmap.put("goodsName", ordergooditem.getGoodsName());
-//                  goodsmap.put("number", ordergooditem.getNumber());
-//                  goodsmap.put("picUrl", ordergooditem.getPicUrl());
-//                  goodsmap.put("specifications", ordergooditem.getSpecifications());
-//                  goodsmap.put("price",ordergooditem.getPrice());
-//                  goodsdata.add(goodsmap);
                     model.orderGoods.add(goodsModel);
                 }
 
                 orders.add(model);
-                //mapdata.put("goodslist",goodsdata);
-                //datalist.add(mapdata);
             }
 
             return ResponseUtil.ok(orders);
@@ -176,7 +159,7 @@ public class OrderController {
         orderVo.put("orderOp", OrderEnum.build(order.getOrderStatus().intValue()));
         orderVo.put("aftersaleStatus", order.getAftersaleStatus());
         orderVo.put("expCode", order.getShipChannel());
-        orderVo.put("expName", expressService.getVendorName(order.getShipChannel()));
+//        orderVo.put("expName", expressService.getVendorName(order.getShipChannel()));
         orderVo.put("expNo", order.getShipSn());
 
         List<ChardancemallOrderGoods> orderGoodsList = orderGoodsService.getByOrderId(order.getOrderSn());
@@ -187,16 +170,17 @@ public class OrderController {
 
         // 订单状态为已发货且物流信息不为空
         //"YTO", "800669400640887922"
-        if (order.getOrderStatus().equals(OrderEnum.IS_DELIVERIED.getCode())) {
-            String expressInfo = expressService.getExpressInfo(order.getShipChannel(), order.getShipSn());
-            if (expressInfo == null) {
-                result.put("expressInfo", new ArrayList<>());
-            } else {
-                result.put("expressInfo", expressInfo);
-            }
-        } else {
-            result.put("expressInfo", new ArrayList<>());
-        }
+        //调用微服务
+//        if (order.getOrderStatus().equals(OrderEnum.IS_DELIVERIED.getCode())) {
+//            String expressInfo = expressService.getExpressInfo(order.getShipChannel(), order.getShipSn());
+//            if (expressInfo == null) {
+//                result.put("expressInfo", new ArrayList<>());
+//            } else {
+//                result.put("expressInfo", expressInfo);
+//            }
+//        } else {
+//            result.put("expressInfo", new ArrayList<>());
+//        }
 
         return ResponseUtil.ok(result);
 
@@ -253,142 +237,142 @@ public class OrderController {
                 return ResponseUtil.fail(ReturnEnumCode.RECEIVE_ADDRESS_EMPTY.getCode(),
                         ReturnEnumCode.RECEIVE_ADDRESS_EMPTY.getMessage());
             }
-
-            ChardancemallAddress address = adressService.getAddressById(addressId);
-            if (address == null) {
-                return ResponseUtil.fail(ReturnEnumCode.RECEIVE_ADDRESS_EMPTY.getCode(),
-                        ReturnEnumCode.RECEIVE_ADDRESS_EMPTY.getMessage());
-            }
-
-            //订单中的商品
-            List<String> productlist = Arrays.asList(productIds.split(","));
-            if (productlist == null || productlist.size() == 0) {
-                return ResponseUtil.badArgument();
-            }
-
-            BigDecimal total = new BigDecimal(0.00);
-            BigDecimal groupprice = new BigDecimal(0.00);
-            List<Integer> idints = productlist.stream().map(Integer::parseInt).collect(Collectors.toList());
-            List<ChardancemallCart> checkedGoodsList = cartService.getCartProductsByUserIdAndIds(userId, idints);
-            //商品或商品品类ID 集合
-            List<Integer> ids = new ArrayList<>();
-            ids.clear();
-
-            List<ChardancemallOrderGoods> orderGoodsList = new ArrayList<>();
-            orderGoodsList.clear();
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            //订单金额
-            BigDecimal orderTotalPrice = new BigDecimal(0.00);
-            //订单中有团购商品
-            BigDecimal checkedGoodsPrice = new BigDecimal(0.00);
-            for (ChardancemallCart cart : checkedGoodsList) {
-
-                ChardancemallGrouponRules grouponRules = grouponRuleService.getGrouponGoodsById(cart.getGoodsId());
-                if (grouponRules != null) {
-                    if (grouponRules.getStatus() == 1 && grouponRules.getExpireTime().isAfter(LocalDateTime.now())) {
-                        return ResponseUtil.fail(ReturnEnumCode.EXIST_GROUPN_EXPIRED_GOODS.getCode(),
-                                ReturnEnumCode.EXIST_GROUPN_EXPIRED_GOODS.getMessage());
-                    }
-                }
-                //只有当团购规格商品ID符合才进行团购优惠
-                if (grouponRules != null && grouponRules.getGoodsId().equals(cart.getGoodsId())) {
-                    groupprice = groupprice.add(grouponRules.getDiscount());
-                    checkedGoodsPrice = checkedGoodsPrice.add(cart.getPrice().subtract(grouponRules.getDiscount()).multiply(new BigDecimal(cart.getNumber())));
-                } else {
-                    checkedGoodsPrice = checkedGoodsPrice.add(cart.getPrice().multiply(new BigDecimal(cart.getNumber())));
-                }
-                ids.add(cart.getGoodsId());
-                //orderTotalPrice = orderTotalPrice.add(cart.getPrice());
-            }
-
-            //优惠券金额
-            BigDecimal plusTotalPrice = new BigDecimal(0.00);
-            orderTotalPrice = checkedGoodsPrice;
-
-            BigDecimal couponPrice = new BigDecimal(0.00);
-
-            //计算优惠券可用情况
-            BigDecimal tmpCouponPrice = new BigDecimal(0.00);
-            Integer tmpCouponId = 0;
-            Integer tmpUserCouponId = 0;
-            int tmpCouponLength = 0;
-            if (!StringUtils.isEmpty(couponId)) {
-                ChardancemallCouponUser usecoupon = userCouponService.getCouponByUserAndCId(userId, couponId);
-                if (usecoupon == null) {
-                    return ResponseUtil.fail(ReturnEnumCode.INVALID_COUPON.getCode(),
-                            ReturnEnumCode.INVALID_COUPON.getMessage());
-                }
-                ChardancemallCoupon coupon = couponService.getCouponById(usecoupon.getCouponId());
-                if (coupon == null) {
-                    return ResponseUtil.fail(ReturnEnumCode.INVALID_COUPON.getCode(),
-                            ReturnEnumCode.INVALID_COUPON.getMessage());
-                }
-
-                if (coupon.getGoodsType() == 1) {
-                    ids = goodsService.getCategoryIdsByGoodsId(ids);
-                }
-
-                //优惠券
-                couponPrice = couponService.checkCouponIsValid(couponId, ids, usecoupon.getAddTime(), checkedGoodsPrice);
-                plusTotalPrice.add(couponPrice);
-            }
-
-            //运费
-            BigDecimal deliveryprice = new BigDecimal(0.00);
-            if (deliveryType != 5) {
-                ChardancemallDeliverycost deliverycost = deliveryCostService.getDeliveryCost(checkedGoodsPrice);
-                if (deliverycost != null) {
-                    deliveryprice = deliverycost.getDeliverycost();
-                }
-            }
-            // 可以使用的其他钱，例如用户积分
-            BigDecimal integralPrice = new BigDecimal(0.00);
-            //ChardancemallIntegralLog integralLog = userIntegralLogService.getUsingIntegralLogByUserId(userId,0);
-            //BigDecimal actualPrice = orderTotalPrice.subtract(plusTotalPrice).add(deliveryprice).subtract(integralPrice);
-            BigDecimal actualPrice = orderTotalPrice.subtract(plusTotalPrice).add(deliveryprice);
-
+            //调用微服务
+//            ChardancemallAddress address = adressService.getAddressById(addressId);
+//            if (address == null) {
+//                return ResponseUtil.fail(ReturnEnumCode.RECEIVE_ADDRESS_EMPTY.getCode(),
+//                        ReturnEnumCode.RECEIVE_ADDRESS_EMPTY.getMessage());
+//            }
+//
+//            //订单中的商品
+//            List<String> productlist = Arrays.asList(productIds.split(","));
+//            if (productlist == null || productlist.size() == 0) {
+//                return ResponseUtil.badArgument();
+//            }
+//
+//            BigDecimal total = new BigDecimal(0.00);
+//            BigDecimal groupprice = new BigDecimal(0.00);
+//            List<Integer> idints = productlist.stream().map(Integer::parseInt).collect(Collectors.toList());
+//            List<ChardancemallCart> checkedGoodsList = cartService.getCartProductsByUserIdAndIds(userId, idints);
+//            //商品或商品品类ID 集合
+//            List<Integer> ids = new ArrayList<>();
+//            ids.clear();
+//
+//            List<ChardancemallOrderGoods> orderGoodsList = new ArrayList<>();
+//            orderGoodsList.clear();
+//
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//            //订单金额
+//            BigDecimal orderTotalPrice = new BigDecimal(0.00);
+//            //订单中有团购商品
+//            BigDecimal checkedGoodsPrice = new BigDecimal(0.00);
+//            for (ChardancemallCart cart : checkedGoodsList) {
+//
+//                ChardancemallGrouponRules grouponRules = grouponRuleService.getGrouponGoodsById(cart.getGoodsId());
+//                if (grouponRules != null) {
+//                    if (grouponRules.getStatus() == 1 && grouponRules.getExpireTime().isAfter(LocalDateTime.now())) {
+//                        return ResponseUtil.fail(ReturnEnumCode.EXIST_GROUPN_EXPIRED_GOODS.getCode(),
+//                                ReturnEnumCode.EXIST_GROUPN_EXPIRED_GOODS.getMessage());
+//                    }
+//                }
+//                //只有当团购规格商品ID符合才进行团购优惠
+//                if (grouponRules != null && grouponRules.getGoodsId().equals(cart.getGoodsId())) {
+//                    groupprice = groupprice.add(grouponRules.getDiscount());
+//                    checkedGoodsPrice = checkedGoodsPrice.add(cart.getPrice().subtract(grouponRules.getDiscount()).multiply(new BigDecimal(cart.getNumber())));
+//                } else {
+//                    checkedGoodsPrice = checkedGoodsPrice.add(cart.getPrice().multiply(new BigDecimal(cart.getNumber())));
+//                }
+//                ids.add(cart.getGoodsId());
+//                //orderTotalPrice = orderTotalPrice.add(cart.getPrice());
+//            }
+//
+//            //优惠券金额
+//            BigDecimal plusTotalPrice = new BigDecimal(0.00);
+//            orderTotalPrice = checkedGoodsPrice;
+//
+//            BigDecimal couponPrice = new BigDecimal(0.00);
+//
+//            //计算优惠券可用情况
+//            BigDecimal tmpCouponPrice = new BigDecimal(0.00);
+//            Integer tmpCouponId = 0;
+//            Integer tmpUserCouponId = 0;
+//            int tmpCouponLength = 0;
+//            if (!StringUtils.isEmpty(couponId)) {
+//                ChardancemallCouponUser usecoupon = userCouponService.getCouponByUserAndCId(userId, couponId);
+//                if (usecoupon == null) {
+//                    return ResponseUtil.fail(ReturnEnumCode.INVALID_COUPON.getCode(),
+//                            ReturnEnumCode.INVALID_COUPON.getMessage());
+//                }
+//                ChardancemallCoupon coupon = couponService.getCouponById(usecoupon.getCouponId());
+//                if (coupon == null) {
+//                    return ResponseUtil.fail(ReturnEnumCode.INVALID_COUPON.getCode(),
+//                            ReturnEnumCode.INVALID_COUPON.getMessage());
+//                }
+//
+//                if (coupon.getGoodsType() == 1) {
+//                    ids = goodsService.getCategoryIdsByGoodsId(ids);
+//                }
+//
+//                //优惠券
+//                couponPrice = couponService.checkCouponIsValid(couponId, ids, usecoupon.getAddTime(), checkedGoodsPrice);
+//                plusTotalPrice.add(couponPrice);
+//            }
+//
+//            //运费
+//            BigDecimal deliveryprice = new BigDecimal(0.00);
+//            if (deliveryType != 5) {
+//                ChardancemallDeliverycost deliverycost = deliveryCostService.getDeliveryCost(checkedGoodsPrice);
+//                if (deliverycost != null) {
+//                    deliveryprice = deliverycost.getDeliverycost();
+//                }
+//            }
+//            // 可以使用的其他钱，例如用户积分
+//            BigDecimal integralPrice = new BigDecimal(0.00);
+//            //ChardancemallIntegralLog integralLog = userIntegralLogService.getUsingIntegralLogByUserId(userId,0);
+//            //BigDecimal actualPrice = orderTotalPrice.subtract(plusTotalPrice).add(deliveryprice).subtract(integralPrice);
+//            BigDecimal actualPrice = orderTotalPrice.subtract(plusTotalPrice).add(deliveryprice);
+//
             DateTimeFormatter df = DateTimeFormatter.ofPattern("YYYMMddHHmmssSSS");
             LocalDateTime dt = LocalDateTime.now();
             String ordersn = dt.format(df);
-            ChardancemallOrder order = new ChardancemallOrder();
-            order.setUserId(userId);
-            order.setOrderSn(ordersn);
-            order.setActualPrice(actualPrice);
-            order.setConsignee(address.getName());
-            order.setMobile(address.getTel());
-            order.setAddress(address.getAddressDetail());
-            order.setMessage(message);
-            order.setGoodsPrice(orderTotalPrice);
-            order.setFreightPrice(deliveryprice);
-            order.setCouponPrice(couponPrice);
-            order.setIntegralPrice(integralPrice);
-            order.setGrouponPrice(groupprice);
-            order.setOrderStatus(OrderEnum.IS_NOT_PAY.getCode().shortValue());
-            order.setOrderPrice(orderTotalPrice.subtract(couponPrice).add(deliveryprice));
-            Integer nret = orderService.addOrder(order);
-            if (nret > 0) {
-                for (ChardancemallCart cart : checkedGoodsList) {
-                    ChardancemallOrderGoods goods = new ChardancemallOrderGoods();
-                    goods.setOrderSn(ordersn);
-                    goods.setGoodsName(cart.getGoodsName());
-                    goods.setGoodsId(cart.getGoodsId());
-                    goods.setGoodsSn(cart.getGoodsSn());
-                    goods.setProductId(cart.getProductId());
-                    goods.setNumber(cart.getNumber());
-                    goods.setPrice(cart.getPrice());
-                    goods.setSpecifications(cart.getSpecifications());
-                    goods.setPicUrl(cart.getPicUrl());
-                    goods.setAddTime(LocalDateTime.now());
-                    goods.setUpdateTime(LocalDateTime.now());
-                    cart.setDeleted(true);
-                    orderGoodsList.add(goods);
-                }
-                orderGoodsService.addBatch(orderGoodsList);
-
-                cartService.updateCartGoodsStatus(true, checkedGoodsList);
+//            ChardancemallOrder order = new ChardancemallOrder();
+//            order.setUserId(userId);
+//            order.setOrderSn(ordersn);
+//            order.setActualPrice(actualPrice);
+//            order.setConsignee(address.getName());
+//            order.setMobile(address.getTel());
+//            order.setAddress(address.getAddressDetail());
+//            order.setMessage(message);
+//            order.setGoodsPrice(orderTotalPrice);
+//            order.setFreightPrice(deliveryprice);
+//            order.setCouponPrice(couponPrice);
+//            order.setIntegralPrice(integralPrice);
+//            order.setGrouponPrice(groupprice);
+//            order.setOrderStatus(OrderEnum.IS_NOT_PAY.getCode().shortValue());
+//            order.setOrderPrice(orderTotalPrice.subtract(couponPrice).add(deliveryprice));
+//            Integer nret = orderService.addOrder(order);
+//            if (nret > 0) {
+//                for (ChardancemallCart cart : checkedGoodsList) {
+//                    ChardancemallOrderGoods goods = new ChardancemallOrderGoods();
+//                    goods.setOrderSn(ordersn);
+//                    goods.setGoodsName(cart.getGoodsName());
+//                    goods.setGoodsId(cart.getGoodsId());
+//                    goods.setGoodsSn(cart.getGoodsSn());
+//                    goods.setProductId(cart.getProductId());
+//                    goods.setNumber(cart.getNumber());
+//                    goods.setPrice(cart.getPrice());
+//                    goods.setSpecifications(cart.getSpecifications());
+//                    goods.setPicUrl(cart.getPicUrl());
+//                    goods.setAddTime(LocalDateTime.now());
+//                    goods.setUpdateTime(LocalDateTime.now());
+//                    cart.setDeleted(true);
+//                    orderGoodsList.add(goods);
+//                }
+//                orderGoodsService.addBatch(orderGoodsList);
+//
+//                cartService.updateCartGoodsStatus(true, checkedGoodsList);
                 return ResponseUtil.ok(ordersn);
-            }
+//            }
         } catch (Exception ex) {
             logger.error("提交订单错误" + ex.getMessage());
         }
